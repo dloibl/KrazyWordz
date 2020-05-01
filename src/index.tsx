e1a79cdc5776bdf2c54e92f52da865b4379011bd
Daniel <daniel.loibl@gmail.com>
2020-05-01 13:32:33 +0200

    add milligram css framework

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { GameView } from "./view/GameView";
-import "./styles/index.css";
 import { DndProvider } from "react-dnd";
 import Backend from "react-dnd-html5-backend";
+
+import "milligram/dist/milligram.css";
+import "./styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
